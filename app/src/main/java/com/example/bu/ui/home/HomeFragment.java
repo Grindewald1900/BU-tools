370df41272c9dc9c380a5551bb911892ef31e7c5
Grindewald1900 <17788108425@164.com>
2020-03-27 01:21:29 -0400

    implement base webview
    Signed-off-by: Grindewald1900 <17788108425@164.com>

--- app/src/main/java/com/example/bu/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/bu/ui/home/HomeFragment.java
@@ -4,8 +4,6 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.webkit.WebSettings;
-import android.webkit.WebView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -15,17 +13,13 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.example.bu.R;
-import com.example.bu.webview.MyWebViewClient;
 
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
-    private WebView mWebView;
-
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        View view = getActivity().getLayoutInflater().inflate(R.layout.fragment_home,null);
         homeViewModel =
                 ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
@@ -36,20 +30,6 @@ public class HomeFragment extends Fragment {
                 textView.setText(s);
             }
         });
-        mWebView = view.findViewById(R.id.web_main);
-        WebSettings webSettings = mWebView.getSettings();
-        webSettings.setJavaScriptEnabled(true);
-        mWebView.setWebViewClient(new MyWebViewClient());
-        mWebView.loadUrl("https://www.ubishops.ca/");
         return root;
     }
-//
-//    @Override
-//    public void onBackPressed(){
-//        if(mWebView.canGoBack()){
-//            mWebView.goBack();
-//        }else {
-//            super.onBackPressed();
-//        }
-//    }
 }
