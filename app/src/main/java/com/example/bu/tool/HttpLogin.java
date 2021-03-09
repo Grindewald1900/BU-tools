2ceeab0ffb87fac784b3111bebefe637db5b3f13
Yee <17788108425@163.com>
2021-03-08 22:41:19 -0500

    Delete some redundant comments

--- app/src/main/java/com/example/bu/tool/HttpLogin.java
+++ app/src/main/java/com/example/bu/tool/HttpLogin.java
@@ -16,25 +16,20 @@ public class HttpLogin {
         String address = "http://10.0.2.2:8080/BU_war_exploded/LoginServlet.do";
         String result = "";
         try{
-            URL url = new URL(address);//初始化URL
+            URL url = new URL(address);
             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-            conn.setRequestMethod("POST");//请求方式
+            conn.setRequestMethod("POST");
 
-            //超时信息
             conn.setReadTimeout(5000);
             conn.setConnectTimeout(5000);
-
-            //post方式不能设置缓存，需手动设置为false
             conn.setUseCaches(false);
 
-            //我们请求的数据
+            // Request data
             String data = "id="+ URLEncoder.encode(id,"UTF-8")+
                     "&password="+ URLEncoder.encode(password,"UTF-8");
             Log.e("BU",data);
 
-            //获取输出流
             OutputStream out = conn.getOutputStream();
-
             out.write(data.getBytes());
             out.flush();
             out.close();
@@ -43,23 +38,15 @@ public class HttpLogin {
             Log.e("BU","Conn_code:"+conn.getResponseCode());
             Log.e("BU","Conn_message:"+conn.getResponseMessage());
             if (conn.getResponseCode() == 200) {
-                // 获取响应的输入流对象
                 InputStream is = conn.getInputStream();
-                // 创建字节输出流对象
                 ByteArrayOutputStream message = new ByteArrayOutputStream();
-                // 定义读取的长度
                 int len = 0;
-                // 定义缓冲区
                 byte buffer[] = new byte[1024];
-                // 按照缓冲区的大小，循环读取
                 while ((len = is.read(buffer)) != -1) {
-                    // 根据读取的长度写入到os对象中
                     message.write(buffer, 0, len);
                 }
-                // 释放资源
                 is.close();
                 message.close();
-                // 返回字符串
                 result = new String(message.toByteArray());
                 Log.e("BU",result);
                 return result;
@@ -79,24 +66,21 @@ public class HttpLogin {
         String result = "";
 
         try{
-            URL url = new URL(address);//初始化URL
+            URL url = new URL(address);
             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-            conn.setRequestMethod("POST");//请求方式
+            conn.setRequestMethod("POST");
 
-            //超时信息
+            //Set up timeout
             conn.setReadTimeout(5000);
             conn.setConnectTimeout(5000);
 
-            //post方式不能设置缓存，需手动设置为false
             conn.setUseCaches(false);
 
-            //我们请求的数据
             String data = "id="+ URLEncoder.encode(id,"UTF-8")+
                     "&password="+ URLEncoder.encode(password,"UTF-8")+
                     "&email="+ URLEncoder.encode(email,"UTF-8");
             Log.e("BU","reg" + data);
 
-            //获取输出流
             OutputStream out = conn.getOutputStream();
 
             out.write(data.getBytes());
@@ -106,23 +90,15 @@ public class HttpLogin {
             Log.e("BU","Conn_code2:"+conn.getResponseCode());
             Log.e("BU","Conn_message2:"+conn.getResponseMessage());
             if (conn.getResponseCode() == 200) {
-                // 获取响应的输入流对象
                 InputStream is = conn.getInputStream();
-                // 创建字节输出流对象
                 ByteArrayOutputStream message = new ByteArrayOutputStream();
-                // 定义读取的长度
                 int len = 0;
-                // 定义缓冲区
                 byte buffer[] = new byte[1024];
-                // 按照缓冲区的大小，循环读取
                 while ((len = is.read(buffer)) != -1) {
-                    // 根据读取的长度写入到os对象中
                     message.write(buffer, 0, len);
                 }
-                // 释放资源
                 is.close();
                 message.close();
-                // 返回字符串
                 result = new String(message.toByteArray());
                 Log.e("BU","reg"+result);
                 //return result;
